302554dd6db94164bb4c45954d2c4ce00616cfea
MCBumblebee <lverbeet18392@gmail.com>
2019-06-09 11:00:49 +0200

    added player model

--- checkerslogic/src/main/java/Model/Player.java
+++ checkerslogic/src/main/java/Model/Player.java
@@ -1,4 +1,29 @@
 package Model;
 
+import Model.GameBoard.Type.PieceColor;
+
 public class Player {
+    private String username;
+    private String Password;
+    private PieceColor myPieces;
+
+    public Player() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public PieceColor getMyPieces() {
+        return myPieces;
+    }
+
+    public void setMyPieces(PieceColor myPieces) {
+        this.myPieces = myPieces;
+    }
 }
